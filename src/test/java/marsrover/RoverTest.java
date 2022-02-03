dcc9061e2d404c8458a83e94386a522a0efa7e72
Craig Kilpatrick <craig.kilpatrick@contino.io>
2022-02-03 09:10:01 -0800

    Updating test class

--- src/test/java/marsrover/RoverTest.java
+++ src/test/java/marsrover/RoverTest.java
@@ -1,18 +1,19 @@
 package marsrover;
 
-import org.junit.Test; 
-import org.junit.Before; 
+import org.junit.Test;
+import org.junit.Before;
 import org.junit.After;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class RoverTest {
+
     @Test
     public void GivenStartingPoint_1_1_N_ThenRoverAt_1_1_N() {
         // arrange
-        Rover rover = new Rover(1,1,'N');
-        int [] expected = new int[]{1,1};
+        Rover rover = new Rover(1, 1, 'N');
+        int[] expected = new int[]{1, 1};
 
         // assert
         assertArrayEquals(expected, rover.getCurrentPosition());
@@ -22,8 +23,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_1_1_N_WhenMoveForward_ThenRoverAt_1_2_N() {
         // arrange
-        Rover rover = new Rover(1,1,'N');
-        int [] expected = new int[]{1,2};
+        Rover rover = new Rover(1, 1, 'N');
+        int[] expected = new int[]{1, 2};
         char[] command = new char[]{'F'};
 
         // act
@@ -37,8 +38,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_1_1_E_WhenMoveForward_ThenRoverAt_2_1_E() {
         // arrange
-        Rover rover = new Rover(1,1,'E');
-        int [] expected = new int[]{2,1};
+        Rover rover = new Rover(1, 1, 'E');
+        int[] expected = new int[]{2, 1};
         char[] command = new char[]{'F'};
 
         // act
@@ -52,8 +53,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_1_2_S_WhenMoveForward_ThenRoverAt_1_1_S() {
         // arrange
-        Rover rover = new Rover(1,2,'S');
-        int [] expected = new int[]{1,1};
+        Rover rover = new Rover(1, 2, 'S');
+        int[] expected = new int[]{1, 1};
         char[] command = new char[]{'F'};
 
         // act
@@ -67,8 +68,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_2_1_W_WhenMoveForward_ThenRoverAt_1_1_W() {
         // arrange
-        Rover rover = new Rover(2,1,'W');
-        int [] expected = new int[]{1,1};
+        Rover rover = new Rover(2, 1, 'W');
+        int[] expected = new int[]{1, 1};
         char[] command = new char[]{'F'};
 
         // act
@@ -79,11 +80,12 @@ public class RoverTest {
         assertEquals('W', rover.getCurrentDirection());
     }
 
+
     @Test
     public void GivenRoverAt_1_2_N_WhenMoveBackward_ThenRoverAt_1_1_N() {
         // arrange
-        Rover rover = new Rover(1,2,'N');
-        int [] expected = new int[]{1,1};
+        Rover rover = new Rover(1, 2, 'N');
+        int[] expected = new int[]{1, 1};
         char[] command = new char[]{'B'};
 
         // act
@@ -97,8 +99,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_2_1_E_WhenMoveBackward_ThenRoverAt_1_1_E() {
         // arrange
-        Rover rover = new Rover(2,1,'E');
-        int [] expected = new int[]{1,1};
+        Rover rover = new Rover(2, 1, 'E');
+        int[] expected = new int[]{1, 1};
         char[] command = new char[]{'B'};
 
         // act
@@ -112,8 +114,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_1_1_S_WhenMoveBackward_ThenRoverAt_1_2_S() {
         // arrange
-        Rover rover = new Rover(1,1,'S');
-        int [] expected = new int[]{1,2};
+        Rover rover = new Rover(1, 1, 'S');
+        int[] expected = new int[]{1, 2};
         char[] command = new char[]{'B'};
 
         // act
@@ -127,8 +129,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_1_1_W_WhenMoveBackward_ThenRoverAt_2_1_W() {
         // arrange
-        Rover rover = new Rover(1,1,'W');
-        int [] expected = new int[]{2,1};
+        Rover rover = new Rover(1, 1, 'W');
+        int[] expected = new int[]{2, 1};
         char[] command = new char[]{'B'};
 
         // act
@@ -142,8 +144,8 @@ public class RoverTest {
     @Test
     public void GivenRoverAt_1_1_N_WhenTurnRight_ThenRoverAt_1_1_W() {
         // arrange
-        Rover rover = new Rover(1,1,'N');
-        int [] expected = new int[]{1,1};
+        Rover rover = new Rover(1, 1, 'N');
+        int[] expected = new int[]{1, 1};
         char[] command = new char[]{'L'};
 
         // act
