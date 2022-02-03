5f089f4c917038d60dc28953964ba3f61facd5a7
Craig Kilpatrick <craig.kilpatrick@contino.io>
2022-02-03 09:41:26 -0800

    Removing tests

--- src/main/java/marsrover/Rover.java
+++ src/main/java/marsrover/Rover.java
@@ -1,51 +1,8 @@
 package marsrover;
 
 public class Rover {
-    int x;
-    int y;
-    char direction;
 
-    Rover(int x, int y, char direction) {
-        this.x = x;
-        this.y = y;
-        this.direction = direction;
-    }
+    Rover() {
 
-    public int[] getCurrentPosition() {
-        return new int[]{x,y};
-    }
-
-    public char getCurrentDirection() {
-        return direction;
-    }
-
-    public void move(char[] command) {
-        for (char cmd: command) {
-            if (cmd == 'F') {
-                if (direction == 'N') {
-                    y++;
-                } else if (direction == 'E') {
-                    x++;
-                } else if (direction == 'S') {
-                    y--;
-                } else if (direction == 'W') {
-                    x--;
-                }
-            } else if (cmd == 'B') {
-                if (direction == 'N') {
-                    y--;
-                } else if (direction == 'E') {
-                    x--;
-                } else if (direction == 'S') {
-                    y++;
-                } else if (direction == 'W') {
-                    x++;
-                }
-            } else if (cmd == 'L') {
-                if (direction == 'N') {
-                    direction = 'W';
-                }
-            }
-        }
     }
 }
